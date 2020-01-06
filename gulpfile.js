3cd86ee33452889e4b22616589f6acd36beee105
AsoTiesto <cute17852337569@gmail.com>
2020-01-06 18:37:02 +0800

    調整功能：隱藏 109 學測題目 trello#kRXOXvk6

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,16 @@ const imageMin = require('gulp-imagemin')
 const pngquant = require('imagemin-pngquant')
 const fs = require('fs').promises
 const path = require('path')
+const gcPub = require("gulp-gcloud-publish");
 
-const distDir = path.join(__dirname, 'dist/')
+const bucketNameForTest = "tutor-test-info";
+const bucketNameForProd = "tutor-infos";
+const projectId = "tutor-204108";
+const projectIdTest = "tutor-test-238709";
+const keyFileName = "tutor.json";
+const keyFileNameTest = "tutor-test.json";
+const projectName = "infos/gsat/";
+
 const findAllUploadFilesPath = async (dir, multiDistEntireFilePath = []) => {
   const files = await fs.readdir(dir)
 
@@ -24,28 +32,43 @@ const findAllUploadFilesPath = async (dir, multiDistEntireFilePath = []) => {
   return multiDistEntireFilePath
 }
 
-const uploadToGCS = async bucketName => {
-  const storage = new Storage({
-    projectId: 'tutor-204108',
-    keyFilename: './tutor.json'
-  })
-
-  const multiDistEntireFilePath = await findAllUploadFilesPath(distDir)
-  multiDistEntireFilePath.forEach(distEntireFilePath => {
-    storage.bucket(bucketName).upload(distEntireFilePath,
-      {
-        destination: `/infos/gsat/${distEntireFilePath.replace(distDir, '')}`,
+let uploadGCSProd = bucketName => {
+  return gulp
+    .src(["dist/**/*"], {
+      base: `${__dirname}/dist/`
+    })
+    .pipe(
+      gcPub({
+        bucket: bucketName,
+        keyFilename: keyFileName,
+        base: projectName,
+        projectId: projectId,
+        public: true,
         metadata: {
-          cacheControl: 'no-cache',
-        },
-        public: true
-      },
-      (err, file) => {
-        console.log(`Upload ${file.name} successfully`)
-      }
-    )
-  })
-}
+          cacheControl: "no-store, no-transform"
+        }
+      })
+    );
+};
+
+let uploadGCSTest = bucketName => {
+  return gulp
+    .src(["dist/**/*"], {
+      base: `${__dirname}/dist/`
+    })
+    .pipe(
+      gcPub({
+        bucket: bucketName,
+        keyFilename: keyFileNameTest,
+        base: projectName,
+        projectId: projectIdTest,
+        public: true,
+        metadata: {
+          cacheControl: "no-store, no-transform"
+        }
+      })
+    );
+};
 
 const minifyImage = sourceImage => {
   return gulp
@@ -61,14 +84,14 @@ const minifyImage = sourceImage => {
 gulp.task('minifyImage', minifyImage.bind(minifyImage, './src/static/img/**/*.@(jpg|png)'))
 
 /* 上傳 GCS */
-gulp.task('uploadToGcsTest', uploadToGCS.bind(uploadToGCS, 'tutor-infos-test/'))
-gulp.task('uploadToGcsProduction', uploadToGCS.bind(uploadToGCS, 'tutor-infos/'))
+gulp.task("uploadGcsTest", uploadGCSTest.bind(uploadGCSTest, bucketNameForTest));
+gulp.task("uploadGcsProd", uploadGCSProd.bind(uploadGCSProd, bucketNameForProd));
 
 /* 部署 */
-gulp.task('deployToTest',
-  gulp.series('minifyImage', 'uploadToGcsTest')
-)
+// gulp.task('deployToTest',
+//   gulp.series('minifyImage', 'uploadToGcsTest')
+// )
 
-gulp.task('deployToProduction',
-  gulp.series('minifyImage', 'uploadToGcsProduction')
-)
+// gulp.task('deployToProduction',
+//   gulp.series('minifyImage', 'uploadToGcsProduction')
+// )
